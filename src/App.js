3e224d7e38b602be76cc501fb5c13ea0e9e27129
Nicolas Guillaume <nefitetifall@gmail.com>
2020-05-11 02:43:55 +0200

    no message

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+import Carte from './componants/meteocard.js'
+
+function App() {
+    return (
+        <div className="App">
+            <Carte/>
+        </div>
+    );
+}
+
+export default App;
